0d47208291e93a4a8b4e290de251013a91734a7a
Saumya Mundra <saumyamundra@gmail.com>
2024-03-09 03:48:44 +0530

    update result

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Array of file keys
 file_keys=(strawberry-platform   
+    airport_v1
 )
 # Download files from S3
-# for key in "${file_keys[@]}"; do
-#     aws s3 cp "s3://unstudio-product-photoshoots/dataset/Lora-Categories/$key" "./data/$key" --recursive
-# done
+for key in "${file_keys[@]}"; do
+    aws s3 cp "s3://unstudio-product-photoshoots/dataset/Lora-Categories/$key" "./data/$key" --recursive
+done
 
 # Run main.py (replace 'main.py' with the actual name of your Python script)
 pip install -r requirements.txt
